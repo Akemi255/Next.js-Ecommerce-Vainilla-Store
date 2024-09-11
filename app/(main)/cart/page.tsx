e00f4d0bb2e33e7118c303054fd753861279220a
gusvet18 <gus1465@yahoo.com>
2024-09-10 19:11:44 -0500

    update

--- app/(main)/cart/page.tsx
+++ app/(main)/cart/page.tsx
@@ -1,32 +1,8 @@
 import { redirect } from "next/navigation";
 
 import CartSection from "./components/cart-section";
-import { auth } from "@/auth";
-import prismadb from "@/lib/prismadb";
 
 export default async function CartPage({ searchParams }: { searchParams: { success: boolean } }) {
-
-    const session = await auth();
-    const userEmail = session?.user?.email ?? undefined;
-
-    if (!session) {
-        redirect("/login")
-    }
-
-    const user = await prismadb.user.findUnique({
-        where: {
-            email: userEmail,
-        },
-    });
-
-    if (!user) {
-        redirect("/reset")
-    }
-
-    if (!user?.emailVerified) {
-        redirect("/verify-email");
-    }
-
 
     if (searchParams.success === true) {
         redirect("payment?success=true")
