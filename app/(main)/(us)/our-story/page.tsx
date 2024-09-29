3f9910ec3662ba799d34a24f93d7f1c956d704e5
gusvet18 <gus1465@yahoo.com>
2024-09-29 13:45:11 -0500

    update

--- app/(main)/(us)/our-story/page.tsx
+++ app/(main)/(us)/our-story/page.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import prismadb from "@/lib/prismadb";
+import Image from "next/image";
 
-export default function OurStoryPage() {
+export default async function OurStoryPage() {
+
+    const aboutText = await prismadb.ourHistory.findFirst({ include: { images: true } });
+    const initialData = aboutText || undefined;
+
+    const urls = initialData?.images.map((image) => image.url);
+
     return (
-        <div className='p-8 max-w-3xl mx-auto'>
-            <h2 className="text-2xl font-semibold mb-3">Our Story</h2>
-            <p className="text-lg">
-                Ancestral Land started as a small venture with a big dream—to bring the best vanilla from around the world to your doorstep. Over the years, we have grown into a trusted name in the industry, known for our dedication to quality and sustainability. From the heart of the vanilla fields to your kitchen, we are here to make your culinary experiences truly extraordinary.
-            </p>
+        <div className="about-us-container mt-3 p-8 max-w-3xl mx-auto bg-white shadow-lg rounded-lg">
+            <h1 className="text-[20px]">{initialData?.text}</h1>
+            {urls && urls.length > 0 && (
+                <div className="mt-6">
+                    <div className="grid grid-cols-2 gap-4">
+                        {urls.map((url, index) => (
+                            <Image width={300} height={300} key={index} src={url} alt={`About Us Image ${index + 1}`} className="rounded-lg shadow-md" />
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
